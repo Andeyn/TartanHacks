349efc49fc4d90a214a2907bf69c62798d039572
timcdot <47467649+timcdot@users.noreply.github.com>
2019-02-09 01:02:23 -0500

    Update OpticalFlow.py

--- OpticalFlow.py
+++ OpticalFlow.py
@@ -1,8 +1,7 @@
 import numpy as np
 import cv2
 import copy
-from PIL import Image
-from PIL import ImageDraw
+
 
 
 def getWebcam():
@@ -10,7 +9,7 @@ def getWebcam():
     #Frame coordinates go frame[y][x]
     while True:
         ret, frame = webcam.read()
-        lowFiFrame = cv2.resize(copy.deepcopy(frame), (0,0), fy=.25, fx=.25)
+        lowFiFrame = cv2.resize(copy.deepcopy(frame), (0,0), fy=.5, fx=.5)
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
