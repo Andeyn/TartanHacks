8fbdc12cb862660abc95e8ed9a84495fc7e43414
Cassie Scheirer <31732188+scheirer-cassie@users.noreply.github.com>
2019-02-09 01:19:41 -0500

    Update OpticalFlow.py

--- OpticalFlow.py
+++ OpticalFlow.py
@@ -1,9 +1,13 @@
 import numpy as np
 import cv2
 import copy
+<<<<<<< HEAD
 import tensorflow
 from tensorflow import emotionanalysis
 import keras_preprocessing
+=======
+
+>>>>>>> 1952e899a76f8e6e7054cbf97f5e5a21ef665538
 
 
 def getWebcam():
@@ -11,7 +15,7 @@ def getWebcam():
     #Frame coordinates go frame[y][x]
     while True:
         ret, frame = webcam.read()
-        lowFiFrame = cv2.resize(copy.deepcopy(frame), (0,0), fy=.25, fx=.25)
+        lowFiFrame = cv2.resize(copy.deepcopy(frame), (0,0), fy=.5, fx=.5)
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
