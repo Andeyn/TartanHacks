86673f785a42369dca0a184da39467edfe662ee3
Andey Ng <andeyng00@gmail.com>
2019-02-08 21:18:13 -0500

    opencv camera

---
+++ OpticalFlow.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+import copy
+from PIL import Image
+from PIL import ImageDraw
+
+
+def getWebcam():
+    webcam = cv2.VideoCapture(0)
+    #Frame coordinates go frame[y][x]
+    while True:
+        ret, frame = webcam.read()
+        lowFiFrame = cv2.resize(copy.deepcopy(frame), (0,0), fy=.25, fx=.25)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    webcam.release()
+    cv2.destroyAllWindows()
+    
+getWebcam()
